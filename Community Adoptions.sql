3a8e546b7941d13ae6df87d00f4fcf4d3e09374e
mattcknight <mattcknight@gmail.com>
2014-05-18 07:34:18 -0500

    Recent Updates

---
+++ Community Adoptions.sql
@@ -0,0 +1,25 @@
+﻿SET TRANSACTION ISOLATION LEVEL READ UNCOMMITTED
+SET NOCOUNT ON
+
+select 
+	MetricDate = CAST(psc.CreateDt AS DATE)
+	,u.GenderGenderSeek
+	,GGS = CASE 
+		WHEN u.GenderGenderSeek = 1 THEN 'Gay Men'
+		WHEN u.GenderGenderSeek = 2 THEN 'Men'
+		WHEN u.GenderGenderSeek = 3 THEN 'Gay Women'
+		WHEN u.GenderGenderSeek = 4 THEN 'Women'
+		END
+	,MetricValue = COUNT(*)
+from [ProfileReadData].[dbo].[proSelfCommunity] psc with (nolock)
+inner join profilereaddata.dbo.users u with (nolock) on psc.userid = u.userid
+group by 
+	CAST(psc.CreateDt AS DATE)
+	,u.GenderGenderSeek
+	,CASE 
+		WHEN u.GenderGenderSeek = 1 THEN 'Gay Men'
+		WHEN u.GenderGenderSeek = 2 THEN 'Men'
+		WHEN u.GenderGenderSeek = 3 THEN 'Gay Women'
+		WHEN u.GenderGenderSeek = 4 THEN 'Women'
+		END
+order by 1,2
---
+++ Price Down Test GT 57 Cohort 1.sql
@@ -0,0 +1,27 @@
+﻿SET TRANSACTION ISOLATION LEVEL READ UNCOMMITTED
+SET NOCOUNT ON
+
+IF OBJECT_ID('tempdb..#tmpPriceDown') IS NOT NULL DROP TABLE #tmpPriceDown
+
+SELECT 
+	[UserGroup]
+	,[RCViews]
+	,[subs]
+	,[totalCash] = CASE 
+		WHEN UserGroup = '0 - Control' THEN CONVERT(DECIMAL(10,2),1.5 * totalCash)
+		ELSE totalCash END
+INTO #tmpPriceDown
+FROM  ReportsDev.dbo.mv_pricedown_o58gen123
+
+SELECT 
+	[UserGroup]
+	,[eliglibleUsers] = SUM([RCViews])
+	,[subs] = SUM([subs])
+	,[totalCash] = SUM(totalCash)
+	,[Conversion] = CONVERT(DECIMAL(10,2),100.00*SUM([subs])/SUM([RCViews]))
+FROM #tmpPriceDown
+GROUP BY [UserGroup]
+ORDER BY 1
+
+DROP TABLE #tmpPriceDown
+-- This is a change for Git
